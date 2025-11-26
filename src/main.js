787d8393d5901e78cde91db89537d8aace9ccf9f
loudenjansen <loudenjansen@gmail.com>
2025-11-26 20:42:49 +0100

    Hook reservations to Supabase on profile

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import './styles/style.css'
-import { initStore, loadFromStorage } from './state/store.js'
+import { initStore, loadFromStorage, loadBoatsFromSupabase } from './state/store.js'
 import { initRouter, navigate } from './ui/router.js'
 import { renderApp } from './ui/render.js'
 
-initStore()
-loadFromStorage()
+async function start(){
+  initStore()
+  loadFromStorage()
+  try {
+    await loadBoatsFromSupabase()
+  } catch(err){
+    console.error('[supabase] startup boats error', err)
+  }
+  initRouter(() => renderApp())
+  navigate('login')
+}
 
-initRouter(() => renderApp())
-navigate('login')
+start()
