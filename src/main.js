cb6ab6000769e76257c31a2a7f9b18f4b061eefe
loudenjansen <loudenjansen@gmail.com>
2025-11-26 21:42:08 +0100

    Add Supabase debug panel and event logging

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import './styles/style.css'
-import { initStore, loadFromStorage } from './state/store.js'
+import { initStore, loadFromStorage, loadBoatsFromSupabase } from './state/store.js'
 import { initRouter, navigate } from './ui/router.js'
 import { renderApp } from './ui/render.js'
+import { initDriftyDebug } from './debug/supabaseDebug.js'
 
-initStore()
-loadFromStorage()
+async function start(){
+  initDriftyDebug()
+  initStore()
+  loadFromStorage()
+  try {
+    await loadBoatsFromSupabase()
+  } catch(err){
+    console.error('[supabase] startup boats error', err)
+  }
+  initRouter(() => renderApp())
+  navigate('login')
+}
 
-initRouter(() => renderApp())
-navigate('login')
+start()
